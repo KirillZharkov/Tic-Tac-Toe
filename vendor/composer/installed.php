ff8b76142788f22a93b555120ee2b3941cc14c6e
KirillZharkov <kiryshazharkov@gmail.com>
2024-10-05 17:45:18 +0300

    Tic-tac-toe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kirillzharkov/tic-tac-toe',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bded276f70d5dbe65f88a911cb2052f2651aabd1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kirillzharkov/tic-tac-toe' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bded276f70d5dbe65f88a911cb2052f2651aabd1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.0',
+            'version' => '0.12.0.0',
+            'reference' => '2d1da4751f47622b56e6616c87b2fb53decee4e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
